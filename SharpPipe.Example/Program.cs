903da0b2475fe94488799a1bf983e5453612d302
cyro <9770110+BlueCyro@users.noreply.github.com>
2025-05-05 18:14:11 -0500

    feat(example)#: Update example program to be a bit more robust and output multiple examples for different filters

--- SharpPipe.Example/Program.cs
+++ SharpPipe.Example/Program.cs
@@ -8,10 +8,41 @@ public class Program
 {
     public static void Main(string[] args)
     {
-        // Make a new SoundPipe object and feed it to the ZitaReverb
+        string inputFile; // Stereo, 32 bit float, 44100hz (use audacity or similar to import/export raw samples)
+
+        if (args.Length == 0)
+        {
+            Console.WriteLine("Please provide a valid file!");
+            return;
+        }
+        else
+        {
+            inputFile = Path.GetFullPath(args[0]);
+
+            if (!File.Exists(args[0]))
+            {
+                Console.WriteLine("File does not exist!");
+                return;
+            }
+        }
+
+        // Make a new SoundPipe object and feed it to the filters.
         using SoundPipe pipe = new(); // Defaults to sample rate of 44100hz
         pipe.SampleRate = 44100; // Set it anyways for demonstration
-        
+
+        ProcessZita(pipe, inputFile);
+        ProcessPitchShift(pipe, inputFile);
+
+        // Print a happy little message to the console.
+        Console.WriteLine("Done!");
+    }
+
+
+    public static void ProcessZita(SoundPipe pipe, string filePath)
+    {
+        byte[] inputFile = File.ReadAllBytes(filePath);
+        Span<float> samples = MemoryMarshal.Cast<byte, float>(inputFile);
+
         // Bathroom preset
         ZitaParameters zitaParams = new()
         {
@@ -36,16 +67,56 @@ public class Program
         zita.FromOther(zitaParams);
 
 
-        // Read a raw file and cast the read bytes to floats
-        byte[] inputFile = File.ReadAllBytes("./Input.raw"); // Stereo, 32 bit float, 44100hz, DTMF tones as a test (use audacity or similar to import/export raw samples)
-        Span<float> samples = MemoryMarshal.Cast<byte, float>(inputFile);
-
         zita.Compute(samples, samples);
 
-        // Print a happy little message to the console.
-        Console.WriteLine("Done!");
+        // Write processed samples back out to a separate raw
+        File.WriteAllBytes(Path.Combine(Path.GetDirectoryName(filePath), "OutputZita.raw"), inputFile);
+    }
+
+
+    public static void ProcessPitchShift(SoundPipe pipe, string filePath)
+    {
+        byte[] inputFile = File.ReadAllBytes(filePath);
+        Span<float> samples = MemoryMarshal.Cast<byte, float>(inputFile);
+
+        // Bathroom preset
+        PitchShiftParameters shifterParams = new()
+        {
+            SemitoneShift = -2f,
+            WindowSize = 512,
+            CrossfadeSamples = 48
+        };
+
+        // Set up pitch shifter objects (pitch shifter is only mono!)
+        PitchShift shifterLeft = new(pipe);
+        PitchShift shifterRight = new(pipe);
+
+        // Since ZitaParameters and ZitaReverb both implement
+        // IZitaFilter, their parameters can be interchanged very easily
+        shifterLeft.FromOther(shifterParams);
+        shifterRight.FromOther(shifterParams);
+
+        int halfLength = samples.Length / 2;
+        float[] left = new float[halfLength];
+        float[] right = new float[halfLength];
+
+        for (int i = 0; i < halfLength; i++)
+        {
+            left[i] = samples[i * 2];
+            right[i] = samples[i * 2 + 1];
+        }
+
+        shifterLeft.Compute(left, left);
+        shifterRight.Compute(right, right);
+
+        for (int i = 0; i < halfLength; i++)
+        {
+             samples[i * 2] = left[i];
+             samples[i * 2 + 1] = right[i];
+        }
+
 
         // Write processed samples back out to a separate raw
-        File.WriteAllBytes("./Output.raw", inputFile);
+        File.WriteAllBytes(Path.Combine(Path.GetDirectoryName(filePath), "OutputPitchShift.raw"), inputFile);
     }
 }
